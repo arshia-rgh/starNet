68ced525f2da29b3989797521816d99135f125cd
arshia <arshiarezagholi1212@gmail.com>
2024-11-17 13:04:56 +0330

    added pkg hash

--- pkg/hash.go
+++ pkg/hash.go
@@ -1,3 +1,17 @@
 package pkg
 
-// TODO: Add hashing functions here
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
+
+func VerifyPassword(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+
+	return err == nil
+}
