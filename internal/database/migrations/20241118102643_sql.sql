66741cfa681fbbc84c2695054a74f579fd3b4e58
arshia <arshiarezagholi1212@gmail.com>
2024-11-18 10:28:37 +0330

    added new migration file

--- internal/database/migrations/20241118102643_sql.sql
+++ internal/database/migrations/20241118102643_sql.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE users (
-    id UUID PRIMARY KEY NOT NULL,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+   id serial PRIMARY KEY  NOT NULL,
+   username VARCHAR(255) NOT NULL UNIQUE,
+   password VARCHAR(255) NOT NULL
 );
 -- +goose StatementEnd
 
